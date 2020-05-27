c90a86bff116dae2f59fe654b71d4a658004983a
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-05-27 13:21:52 +0200

    Updates

---
+++ precache-manifest.88abaf43f55ff875ad138dd219e61d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47035bbdc3b7958f18e3c53a833e8aba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58fe43d7a19f2a1aed96",
+    "url": "/monsters-rolodex/static/css/main.1cc9d2af.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fe43d7a19f2a1aed96",
+    "url": "/monsters-rolodex/static/js/main.3d18042c.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
